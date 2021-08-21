bd365972354e6bc893030814b096393a3fb7bebf
SangjunYou <fbtkdwns12@gmail.com>
2021-08-21 17:59:36 +0900

    .

---
+++ Web scraping/study/16_selenium_movies_scroll.py
@@ -0,0 +1,74 @@
+
+
+from selenium import webdriver
+browser = webdriver.Chrome()
+browser.maximize_window()
+
+# 페이지 이동
+url = "https://play.google.com/store/movies/top"
+browser.get(url)
+
+# 지정한 위치로 스크롤 내리기
+# 모니터(해상도) 높이인 1080 위치로 스크롤 내리기
+# browser.execute_script("window.scrollTo(0, 1080)")
+
+# 화면 가장 아래로 스크롤 내리기
+# browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+
+import time
+interval = 2   # 2초에 한번씩 스크롤 내림
+
+# 현제 문서 높이를 가져와서 저장
+prev_height = browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+
+# 반복수행
+while True:
+    # 스크롤 가장 아래로 내림
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+
+    # 페이지 로딩 대기
+    time.sleep(interval)
+
+    # 현제 문서 높이를 가져와서 저장
+    crr_height = browser.execute_script("return document.body.scrollHeight")
+
+    if crr_height == prev_height:
+        break
+    
+    prev_height = crr_height
+
+print("스크롤 완료")
+
+import requests 
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(browser.page_source, "lxml")
+
+movies = soup.find_all("div", attrs={"class":"Vpfmgd"})
+print(len(movies))
+
+for movie in movies:
+    title = movie.find("div", attrs={"class":"WsMG1c nnK0zc"}).get_text()
+    # print(title)
+
+    # 할인 전 가격
+    original_price = movie.find("span", attrs={"class":"SUZt4c djCuy"})
+    if original_price:
+        original_price = original_price.get_text()
+    else:
+        # print(title,"   <할인되지 않는 영화는 제외>")
+        continue
+    
+    # 할인 된 가격
+    price = movie.find("span", attrs={"class":"VfPpfd ZdBevf i5DZme"}).get_text()
+
+    link = movie.find("a", attrs={"class":"JC71ub"})["href"]
+
+    # 올바른 링크 : https://play.google.com/ + link
+    print(f"제목 : {title}")
+    print(f"할인 전 금액 : {original_price}")
+    print(f"할인 후 금액 : {price}")
+    print("링크 : ", "https://play.google.com" + link)
+    print("_"*100)
+
+browser.quit()
